890c54918d4304d15f68885b59c3f2f725a7fd64
luigidibella <lykos.omega@gmail.com>
2024-04-03 16:03:35 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script>
+import axios from 'axios';
+import { store } from './data/store';
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  components:{
+    Header,
+    Main,
+    Footer
+  },
+
+  data(){
+    return{
+      store,
+    }
+  },
+
+  methods:{
+    getApi(){
+      console.log('GET API');
+    }
+    
+  },
+
+  mounted(){
+    this.getApi()
+  }
+
+}
+
+</script>
+
+
+
+<template>
+
+  <Header />
+
+  <Main />
+
+  <Footer />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/data/store.js
@@ -0,0 +1,5 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+  apiUrl:'https://api.themoviedb.org/3/movie/550?api_key=e60a33cd49ffc066d107162f1e1e846f',
+})
