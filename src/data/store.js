6c446963709010d19bc4f125e6627055f3b72999
LuigiDiBella <lykos.omega@gmail.com>
2024-08-21 02:16:46 +0200

    [add] apiUrlPopolarMovie

--- src/data/store.js
+++ src/data/store.js
@@ -1,12 +1,16 @@
 import { reactive } from "vue";
 
 export const store = reactive({
+  apiUrlPopolarMovie: 'https://api.themoviedb.org/3/movie/popular',
   apiUrl:'https://api.themoviedb.org/3/search/',
+  
   queryParams:{
     api_key: 'e60a33cd49ffc066d107162f1e1e846f',
     language: 'it-IT',
     query: ''
   },
+
+  popular: [],
   movie: [],
   tv: [],
 })
