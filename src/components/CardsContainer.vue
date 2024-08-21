e6192c06f2ba71a0ce0c14f5b36a3ae88e374fee
LuigiDiBella <lykos.omega@gmail.com>
2024-08-21 02:14:51 +0200

    [add] popular

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -18,15 +18,18 @@ export default {
 
   computed:{
     title(){
-      return this.type === 'movie' ? 'Film' : 'Serie TV'
+      if(this.type === 'popular') {
+        return 'Nuovi e popolari'
+      }
+        return this.type === 'movie' ? 'Film' : 'Serie TV'
     }
   }
 }
 </script>
 
 <template>
-  <div class="container text-center text-white">
-    <h1 class="text-danger text-uppercase">{{ title }}</h1>
+  <div class="container text-white">
+    <h1 class="">{{ title }}</h1>
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
       <MyCard
         v-for="card in store[type]"
@@ -42,6 +45,11 @@ export default {
   background-color: #434343;
   &:first-of-type{
     padding-top: 100px;
+  }
+  h1{
+    border-left: 5px solid red;
+    padding-left: 15px;
+    margin-bottom: 15px;
   }
 }
 </style>
